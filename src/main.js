578becb9c887dab3b8cd45975074b1f62f3df320
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2021-06-03 16:08:45 +0200

    installed primevue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+// import router from './router';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Password from 'primevue/password';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
 
-createApp(App).mount('#app')
+const app = createApp(App).
+            //   use(router).
+              use(PrimeVue, { 
+                  ripple: true,
+                  locale: {
+                      weak: "Svagt",
+                      medium: "Medium",
+                      strong: "Starkt"
+                  }
+             });
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Password', Password);
+
+app.mount("#app");
